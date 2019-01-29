a4aabbc00e13d5f9fdabf402b29cd0cdaec254b5
Vincent Restrepo <vdrestrepo@geosat.com.co>
2019-01-29 12:05:38 -0500

    Agregada bobadita para que el botón cambie de color cuando se presiona en él

--- app/src/main/java/com/vince/kotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/vince/kotlinandroidapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.vince.kotlinandroidapp
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity(), TextWatcher {
 
@@ -13,6 +17,8 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     private var editAge: EditText? = null
     private var textName: TextView? = null
     private var textAge: TextView? = null
+    private var buttonExecute: Button? = null
+    private var buttonState: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +28,11 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         editAge = findViewById(R.id.editText_age) as EditText
         textName = findViewById(R.id.textView_name) as TextView
         textAge = findViewById(R.id.textView_age) as TextView
+        buttonExecute = findViewById(R.id.btn_execute) as Button
 
         editName!!.addTextChangedListener(this) // !! asegura que no tenemos ningún null
         editAge!!.addTextChangedListener(this)
+        buttonExecute?.setOnClickListener { changeButtonColor() }
 
     }
 
@@ -39,8 +47,20 @@ class MainActivity : AppCompatActivity(), TextWatcher {
     }
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        textName?.text = editName?.text.toString()
-        textAge?.text = editAge?.text.toString()
+        textName?.text = editName?.text
+        textAge?.text = editAge?.text
+    }
+
+    fun changeButtonColor(){
+        if(buttonState == 0) {
+            buttonState = 1
+            buttonExecute!!.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+            buttonExecute!!.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorOrange))
+        } else {
+            buttonState = 0
+            buttonExecute!!.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorOrange))
+            buttonExecute!!.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+        }
     }
 
 
