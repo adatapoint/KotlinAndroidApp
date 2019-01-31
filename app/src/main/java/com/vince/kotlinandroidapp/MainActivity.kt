b7b3b84e5ac4c1b383f72653022d6a751b493a5c
Vincent Restrepo <vdrestrepo@geosat.com.co>
2019-01-31 17:48:56 -0500

    Asuntos varios de comparaciones en Kotlin

--- app/src/main/java/com/vince/kotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/vince/kotlinandroidapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,13 +16,13 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
 
 
+    private val WHITE = "White"
+    private val ORANGE = "Orange"
     private var editName: EditText? = null // ? cuando no lo pueda inicializar con otra cosa.
     private var editAge: EditText? = null
     private var textName: TextView? = null
     private var textAge: TextView? = null
     private var buttonExecute: Button? = null
-    private var WHITE: String = "White"
-    private var ORANGE: String = "Orange"
     private var buttonColor: String = ORANGE
     private var button: Button? = null
 
@@ -71,7 +72,50 @@ class MainActivity : AppCompatActivity(), TextWatcher, View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show()
+        operacion()
+    }
+
+    fun operacion(){
+        val valor1 = 1
+        val valor2 = 2
+        val valor3 = 3
+
+        var entero = valor1.plus(valor2).minus(valor3).times(valor1).div(valor2).plus(valor2.plus(valor3).times(valor2)).rem(valor2) //rem %
+        entero++
+        entero--
+        entero.inc()
+        entero.dec()
+        entero += valor2
+        entero -= valor1
+        entero *= valor2
+        entero /= valor1
+        entero %= valor1
+
+        var bool = false
+
+        if(bool.not()) {
+            entero = 30
+        }
+
+        if(valor2.equals(valor1)){
+            entero = 0
+        }
+
+
+
+        var test = null
+        var otroTest = "otro test"
+        var testResult = test?.equals(otroTest) ?: (otroTest == null) // De una comparación falsa, pasa a evaluar la otra.
+
+        entero = 2.compareTo(1) // 0 si es igual, < 0 si el primero es menor, > 0 si el primero es mayor.
+
+        var result = 2.compareTo(1) >= 0
+
+        Log.i("asdf", valor1.toString())
+        Log.i("asdf", valor2.toString())
+        Log.i("asdf", entero.toString())
+
+        Toast.makeText(this,"El valor es $entero y $testResult y $result !!", Toast.LENGTH_SHORT).show()
     }
 
 }
